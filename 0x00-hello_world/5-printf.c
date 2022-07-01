e97100d2debbd8d71edc3f4b37b2ef55d8bd927a
tochukwumaduka <davemaduluv@gmail.com>
2022-07-01 07:00:29 -0700

    go

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (success)
@@ -6,5 +7,6 @@
  int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 }
