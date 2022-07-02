9aaa3ee139286f9118abac13cdb36e2025fc982d
tochukwumaduka <davemaduluv@gmail.com>
2022-07-02 09:33:38 -0700

    committing 0 and 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,17 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - Task 1 print the last digit of the number stored in the variable n*
+*main - assign a random number to the variable n each time it is executed
 *
  *Return: 0
  **/
 	int main(void)
-
-	{
+{
 
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 	printf("%d is %s\n", n, "positive");
@@ -25,8 +23,7 @@
 	}
 	else
 	{
-	printf("d is %s\n", n, "zero");
+	printf("%d is %s\n", n, "zero");
 	}
 	return (0);
-
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,31 +8,26 @@
  **/
 
 int main(void)
-/*return value of main = 0 */
-	{
+{
 
 	int n;
 	char last[] = "last digit of ";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/*my code is here*/
 	printf("%s %d is %d and is ", last, n, n % 10);
-
 	if (n % 10 > 5)
 	{
-
 	printf("greater than 5\n");
 	}
-
 	else if (n % 10 == 0)
 	{
 	printf("0\n");
 	}
-
 	else
 	{
 	printf("less than 6 and not 0\n");
 	}
 	return (0);
-/* return -> success always 0 */
-	}
+}
