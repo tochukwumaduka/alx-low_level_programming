497be8e00b4726b0456f49e89b61945269d0ac09
tochukwumaduka <davemaduluv@gmail.com>
2022-07-05 21:41:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - print alphabet
+*Return: no return statement
+*/
+
+void print_alphabet(void);
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
