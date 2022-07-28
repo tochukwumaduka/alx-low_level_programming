e8153914c12414cf47680bbe753d176a2dc17f85
tochukwumaduka <davemaduluv@gmail.com>
2022-07-27 19:48:05 -0700

    task 2 correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,4 +31,9 @@ char *str_concat(char *s1, char *s2)
 
 	for (index = 0; s1[index]; index++)
 		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
