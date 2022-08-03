66e0246a1d893016bc0ed41383b719e7d01baced
tochukwumaduka <davemaduluv@gmail.com>
2022-08-02 19:25:19 -0700

    hot fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,14 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
- * main - Prints the name of the file it was compiled from
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
  *
- * Return: always 0.
+ * Return: Always 0.
  */
-
 int main(void)
-	{
-		printf("%s\n", __FILE__);
+{
+	printf("%s\n", __FILE__);
 
-		return (0);
-	}
+	return (0);
+}
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,10 +2,9 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /*
- * file: 3-FUNCTION_LIKE_MACRO.h
- * Desc: header fie that defines a function-like macro ABS(x)
- * that computes the absolute value of x.
- *
+ * File: 3-function_like_macro.h
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
